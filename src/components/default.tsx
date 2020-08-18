58c4c8dcdee41f7ff3833f9fab0bafbb88125efc
Tiago Marques <tiagomapmarques@users.noreply.github.com>
2020-08-18 15:48:36 +0100

    Bugfix: Component props (#14)
    * fix: component props
    
    * chore: address pr comments

--- src/components/default.tsx
+++ src/components/default.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { ConversionService } from '../services';
 import { AnyProps } from './types';
 
+const shouldBePrinted = (value: unknown): boolean => !(
+  value === null || value === undefined || value === false
+);
+
+const toHTMLAttribute = (value: unknown): string => (
+  value instanceof Object ? JSON.stringify(value) : value.toString()
+);
+
 export const Default = ({ blok }: AnyProps): JSX.Element => {
   const props = Object.keys(blok)
     .filter((key) => key && key[0] !== '_' && key !== 'component')
     .reduce((accumulator, key) => ({
       ...accumulator,
-      [key]: blok[key],
+      ...(shouldBePrinted(blok[key]) ? {
+        [ConversionService.camelToKebab(key)]: toHTMLAttribute(blok[key]),
+      } : {}),
     }), {});
 
   const CustomComponent = ConversionService.camelToKebab(blok.component);

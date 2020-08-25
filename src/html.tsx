7d760cc0fe0075c4c6eaa7d538c773ec280e33a6
Tiago Marques <tiagomapmarques@users.noreply.github.com>
2020-08-25 11:28:00 +0100

    Bugfix: Global stylesheet (#18)
    * fix: add global stylesheet
    
    * fix: nomodule script

--- src/html.tsx
+++ src/html.tsx
@@ -11,16 +11,16 @@ interface HTMLProps {
 
 // eslint-disable-next-line import/no-default-export
 export default function HTML({ body, headComponents, postBodyComponents }: HTMLProps): JSX.Element {
-  const srcScript = `${getComponentsURL()}/roche-component-library/roche-component-library.esm.js`;
+  const src = `${getComponentsURL()}/roche-component-library/roche-component-library`;
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {headComponents}
-        {/** src matches stencil component library's development server /dist */}
-        <script type="module" src={srcScript}></script>
-        <script noModule src={srcScript.replace('.esm.js', '.js')}></script>
+        <link rel="stylesheet" href={`${src}.css`} />
+        <script type="module" src={`${src}.esm.js`}></script>
+        <script noModule src={`${src}.js`}></script>
       </head>
       <body>
         <div id="___gatsby" dangerouslySetInnerHTML={{ __html: body }} />

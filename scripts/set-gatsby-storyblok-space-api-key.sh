5d481b1f97e0db98b5957388fc539f7968128a48
Anderson Carvalho <anderson.carvalho@virtual-identity.com>
2020-09-15 08:47:36 +0100

    chore: fetch apikeys from aws secrets manager and defined the apikey name for each workflow (#31)

---
+++ scripts/set-gatsby-storyblok-space-api-key.sh
@@ -0,0 +1,5 @@
+GATSBY_STORYBLOK_SPACE_API_KEY=$(\
+  aws secretsmanager \
+      get-secret-value --secret-id $STORYBLOK_SPACE_NAME-secret \
+      | jq '.SecretString | fromjson' \
+      | jq '."'${STORYBLOK_SPACE_NAME}.apikeys.${STORYBLOK_API_KEY_NAME}'"' -r)

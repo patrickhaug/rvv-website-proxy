299be60833fe231e25bfcbdebae05ac75d2602f5
César Marques <cesar.marques@virtual-identity.com>
2020-10-01 22:49:58 +0100

    feat: add link parsing (#46)
    Co-authored-by: César Marques <cesar.marques@virtual-identity.com>

--- src/components/props/index.ts
+++ src/components/props/index.ts
@@ -1,6 +1,7 @@
 import { StringService } from '../../services';
 import { asset } from './asset';
 import { multiAsset } from './multi-asset';
+import { multiLink } from './multi-link';
 
 const defaultMapper = (key: string, value: unknown): Record<string, string> => ({
   [StringService.snakeToKebab(key)]: value instanceof Object
@@ -15,6 +16,9 @@ export const getMappedProps = (key: string, blok: any): Record<string, string> =
   }
   if (blok.fieldtype === 'asset') {
     return asset(key, blok);
+  }
+  if (blok.fieldtype === 'multilink') {
+    return multiLink(key, blok);
   }
 
   return defaultMapper(key, blok);
---
+++ src/components/props/multi-link.ts
@@ -0,0 +1,20 @@
+import { StringService } from '../../services';
+
+export interface MultiLinkData {
+  anchor: string;
+  cached_url: string;
+  id: string;
+  linktype?: string;
+  url?: string;
+  fieldtype?: string;
+}
+
+const rootAlias = 'home';
+
+export const multiLink = (key: string, data: MultiLinkData): Record<string, string> => {
+  /* eslint-disable @typescript-eslint/camelcase */
+  const { cached_url, anchor, url } = data;
+  const parsedLink = url || `/${cached_url.replace(rootAlias, '')}${anchor ? `#${anchor}` : ''}`;
+  /* eslint-enable @typescript-eslint/camelcase */
+  return ({ [StringService.snakeToKebab(key)]: parsedLink });
+};

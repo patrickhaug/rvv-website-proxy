4dfd8e3691410bea09d5917288708cb426f3056c
Anderson Carvalho <anderson.mo.carvalho@gmail.com>
2020-10-24 21:04:30 +0100

    fix: webhooks api used inside the scripts (#58)

--- scripts/emit-event-live-deploy-started.sh
+++ scripts/emit-event-live-deploy-started.sh
@@ -1,4 +1,4 @@
 curl -X POST \
-  "${ROCHE_WEBHOOKS_API_URL}/v1/storyblok-event-notifications" \
+  "${WEBHOOKS_API_URL}/v1/storyblok-event-notifications" \
   -H 'Content-Type: application/json' \
   -d "{\"action\":\"live_deploy_started\", \"space_id\": ${STORYBLOK_SPACE_ID} }"

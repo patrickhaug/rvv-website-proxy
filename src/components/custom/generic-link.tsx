3103387a18c0600bea5ce332a83218f7319e69f4
Kai Wissler <kai.wissler@virtual-identity.com>
2021-03-29 10:01:48 +0200

    feat: extend link with download asset

--- src/components/custom/generic-link.tsx
+++ src/components/custom/generic-link.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AnyProps } from '../types';
 import { flattenProps } from '../../services/component-mapping';
+import { asset } from '../props/asset';
 
 const GenericLinkComponents = {
   'rcm-generic-link': {
@@ -10,18 +11,28 @@ const GenericLinkComponents = {
       target: '_blank',
       icon: 'download',
     },
-    mappings: {
-      url: 'src', // type: asset
-    },
   },
 };
 
-const getRcmGenericLink = (comp): React.ElementType => {
+const getRcmGenericLink = (compName: string): React.ElementType => {
+  const comp = GenericLinkComponents[compName];
   // giving the function a name is just to have named mappings for debugging purposes
+  const RcmGenericDownloadLink = ({ blok, slot }: AnyProps): JSX.Element => React.createElement(
+    'rcm-generic-link',
+    {
+      ...flattenProps(comp, blok), ...comp.props, ...asset('url', blok.url[0].asset), slot,
+    },
+  );
   const RcmGenericLink = ({ blok, slot }: AnyProps): JSX.Element => React.createElement(
     'rcm-generic-link',
-    { ...flattenProps(comp, blok), ...comp.props, slot },
+    {
+      ...flattenProps(comp, blok), ...comp.props, slot,
+    },
   );
+
+  if (compName === 'rcm-download-link') {
+    return RcmGenericDownloadLink;
+  }
 
   return RcmGenericLink;
 };
@@ -29,5 +40,5 @@ const getRcmGenericLink = (comp): React.ElementType => {
 export const rcmGenericLinkComponents = Object.keys(GenericLinkComponents)
   .reduce((accumulator, componentName) => ({
     ...accumulator,
-    [componentName]: getRcmGenericLink(GenericLinkComponents[componentName]),
+    [componentName]: getRcmGenericLink(componentName),
   }), {});

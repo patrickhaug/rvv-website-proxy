b081be0c79f14f8578d23239e2ff4596eaa1df87
Rebecca Emmanuel <rebeccaemmanuel1995@gmail.com>
2021-07-21 22:04:00 +0100

    fix(global config): add use effect

--- src/components/custom/global-config.tsx
+++ src/components/custom/global-config.tsx
@@ -1,10 +1,12 @@
-import React, { ReactType } from 'react';
+import React, { ReactType, useEffect } from 'react';
 import { GlobalConfigProps, StringService, LanguageService } from '../../services';
 
 const CustomComponent = 'rcm-global-config' as ReactType;
 
 export const RcmGlobalConfig = (props: GlobalConfigProps): JSX.Element => {
-  document.documentElement.lang = props.locale || LanguageService.defaultLocale;
+  useEffect(() => {
+    document.documentElement.lang = props.locale || LanguageService.defaultLocale;
+  });
 
   const newProps = Object.keys(props).reduce((accumulator, prop) => ({
     ...accumulator,

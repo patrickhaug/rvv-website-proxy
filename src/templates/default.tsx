ecef686a4cbe271883437d375b08a2fff5e83b98
Julia Vorcakova <julia.vorcakova@viable.at>
2021-07-30 11:31:56 +0200

    feat(contact): add contact button

--- src/templates/default.tsx
+++ src/templates/default.tsx
@@ -65,6 +65,7 @@ const Article = 'rcm-layout-article' as React.ElementType;
 const Container = 'rcm-layout-container' as React.ElementType;
 const FundsList = 'rcm-layout-funds' as React.ElementType;
 const FundsDetail = 'rcm-layout-fund' as React.ElementType;
+const ContactButton = 'rcm-contact-button' as React.ElementType;
 
 // eslint-disable-next-line import/no-default-export
 export default class StoryblokEntry extends Component<StoryblokEntryProps, StoryblokEntryState> {
@@ -176,6 +177,10 @@ export default class StoryblokEntry extends Component<StoryblokEntryProps, Story
           }
           {story.content.component !== 'article' && blokToComponent({ blok: story.content, getComponent })}
         </Container>
+        <ContactButton
+          link={globalContent?.contact?.button?.link}
+          name={globalContent?.contact?.button?.name}>
+        </ContactButton>
         <Footer
           tree={navigation}
           getComponent={getComponent}
